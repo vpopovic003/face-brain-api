694d0f6449afdaa0c0776958b5fc63d3875bb967
keen <keen@pop-os.localdomain>
2023-08-06 16:46:29 +0200

    fix port message

--- server.js
+++ server.js
@@ -100,5 +100,5 @@ app.put('/image', (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('app runing on port 3000');
+  console.log(`app runing on port ${process.env.PORT}`);
 })
