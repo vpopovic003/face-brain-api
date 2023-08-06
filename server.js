8df33999697a40328681c902d90dc8f687ebf1ce
keen <keen@pop-os.localdomain>
2023-08-06 16:35:12 +0200

    add environment PORT

--- server.js
+++ server.js
@@ -99,6 +99,6 @@ app.put('/image', (req, res) => {
   .catch(err => res.status(400).json('unable to get entries'))
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('app runing on port 3000');
 })
