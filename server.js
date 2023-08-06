1a8d870bf22136c2922f72686ca703b676c7dd85
keen <keen@pop-os.localdomain>
2023-08-06 17:02:06 +0200

    update environment variables for db

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false},
+    host : process.env.DATABASE_HOST,
     port : 5432,
-    user : 'keen',
-    password : 'qwe',
-    database : 'face-brain'
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PW,
+    database : process.env.DATABASE_DB
   }
 });
 
